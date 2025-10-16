0573d10a3588922bd90869c7dd776ed6e7b5f3c2
younisY24 <hhhyounis22@gmail.com>
2025-10-16 15:16:30 +0300

    Update F.js

--- F.js
+++ F.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'Market-v2';
+const CACHE_NAME = 'fitness-v1';
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache =>
-cache.addAll(['/', '/ar/index.html', '/ar/manifest.json', '/ar/compounding_calculator.html', '/ar/contract.html', '/ar/index.css', '/ar/index.js', '/ar/app.js', '/ar/lot.js', '/ar/calcr.js', '/images/0x.png', '/images/0x1.png', '/images/0x3.png', '/images/0x4.png', '/images/0x5.png', '/images/0x6.png', '/images/0x7.png', '/images/0x8.png', '/images/0x9.png', '/images/0x10.png', '/images/0x11.png', '/images/0xbtc.png', '/images/btc.png', '/images/btc21.png', '/images/ltc.png', '/images/paxg.png']);    )
+cache.addAll(['/', '/ar/index.html', '/ar/manifest.json', '/ar/compounding_calculator.html', ']);    )
   );
 });
 
